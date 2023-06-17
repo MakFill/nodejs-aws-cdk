16f51b9461ccb92b8f30989464b6ac21eb2e90e5
MakFil <makfil1177@gmail.com>
2023-06-17 21:50:56 +0300

    [REFACTOR] Integration with s3 FE side

--- FE/src/constants/apiPaths.ts
+++ FE/src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://c8yb12v07b.execute-api.eu-west-1.amazonaws.com",
   bff: "https://qqk4lyq4l7.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
