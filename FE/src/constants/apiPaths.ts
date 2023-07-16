5011fb475a69b66a6cf72609dbbd0241252ecf1c
MakFil <makfil1177@gmail.com>
2023-07-16 15:15:58 +0300

    [FEAT] Integrate cart service

--- FE/src/constants/apiPaths.ts
+++ FE/src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://cuarmb5yc7.execute-api.eu-west-1.amazonaws.com/prod",
   bff: "https://vofeuy0236.execute-api.eu-west-1.amazonaws.com",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://4xz2p6nre8.execute-api.eu-west-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
