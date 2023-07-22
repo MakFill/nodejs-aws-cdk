019dd2f91894bdfedf57d7e4774c46683405c425
MakFil <makfil1177@gmail.com>
2023-07-22 13:49:12 +0300

    [FEAT] Implement bff logic

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { All, Controller, Header, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @All('*')
+  @Header('content-type', 'application/json')
+  async getResponse(@Res() res: Response): Promise<void> {
+    const response = await this.appService.getResponse();
+    res.status(response.status).json(response.data);
   }
 }
--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT ?? 4001;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(PORT, () =>
+    console.log(`Server listening on port: ${PORT}`),
+  );
 }
 bootstrap();
