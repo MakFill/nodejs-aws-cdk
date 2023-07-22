b1f2e1b313dcc5f327d201e28a44a8cbe7daaef7
MakFil <makfil1177@gmail.com>
2023-07-22 19:33:09 +0300

    [FEAT] Update FE part with proxy endpoint

--- FE/src/constants/apiPaths.ts
+++ FE/src/constants/apiPaths.ts
@@ -1,8 +1,11 @@
+const BFF_LINK = "http://makfill-bff-api-prod.eu-west-1.elasticbeanstalk.com/";
+const BFF_PROXY = "https://e3k7vbodzg.execute-api.eu-west-1.amazonaws.com";
+
 const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://cuarmb5yc7.execute-api.eu-west-1.amazonaws.com/prod",
-  bff: "https://vofeuy0236.execute-api.eu-west-1.amazonaws.com",
-  cart: "https://4xz2p6nre8.execute-api.eu-west-1.amazonaws.com/api",
+  import: `${BFF_PROXY}/import`,
+  bff: `${BFF_PROXY}/product`,
+  cart: `${BFF_PROXY}/cart`,
 };
 
 export default API_PATHS;
